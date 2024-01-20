3ffa5b93ab5424c8442015f1fb6b27c1834f448f
Gustavo Bodra <gustavo.bodra@startse.com>
2024-01-20 19:15:09 -0300

    Refactor pose.py to calculate and display movement angle

--- pose.py
+++ pose.py
@@ -1,15 +1,17 @@
+import time
 import cv2
 import calculate_angles
 import mediapipe as mp
+from datetime import timedelta
 
+start_time = time.time()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose 
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 incorrect_angle_counter = 0
 
-outdir, inputflnm = './', 'vids/PXL_20221119_130822683.mp4'
-# outdir, inputflnm = './', 'vids/PXL_20221119_132147650_2.mp4'
+outdir, inputflnm = './', 'vids/VID-20230704-WA0005.mp4'
 
 cap = cv2.VideoCapture(inputflnm)
 
@@ -21,52 +23,53 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 inflnm, inflext = inputflnm.split('.')
 out_filename = f'{outdir}{inflnm}_annotated_w_angle.mp4'
-out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 40, (frame_width, frame_height))
+out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc('H', '2', '6', '4'), 30, (frame_width, frame_height))
+# out = cv2.VideoWriter(out_filename, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 40, (frame_width, frame_height))
 
 while cap.isOpened():
     ret, image = cap.read()
-    image = cv2.flip(image, 0)
 
     if not ret:
         break
     
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = pose.process(image)
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)      
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    # last_point = results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x
-    angle = calculate_angles.get_angle(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP],
-                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE],
-                                       results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE])
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    if results.pose_landmarks is not None:
+        angle = calculate_angles.get_angle(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP],
+                                           results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE],
+                                           results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER])
 
-    # org
-    position = (50, frame_height - 50)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        position = (50, frame_height - 50)
+        fontScale = 1
 
-    # fontScale
-    fontScale = 1
+        if angle <= 0.0:
+            color = (0, 0, 255)
+            incorrect_angle_counter = incorrect_angle_counter + 1
+        else:
+            color = (255, 0, 0)
 
-    # Blue color in BGR
-    if angle <= 100.0:
-        # vermelho
-        color = (0, 0, 255)
-        incorrect_angle_counter = incorrect_angle_counter + 1
-    else:
-        # azul
-        color = (255, 0, 0)
+        thickness = 2
+        img_text = 'Angulo Movimento: ' + str(angle) + ' | Movimentos Incorretos: ' + str(incorrect_angle_counter)
+        
+        # Calculate the width and height of the text box
+        (text_width, text_height) = cv2.getTextSize(img_text, font, fontScale, thickness)[0]
+        box_coords = ((position[0] - 5, position[1] + 5), (position[0] + text_width + 5, position[1] - text_height - 5))
+        cv2.rectangle(image, box_coords[0], box_coords[1], (255, 255, 255), cv2.FILLED)
 
-    # Line thickness of 2 px
-    thickness = 2
-
-    # Using cv2.putText() method
-    img_text = 'Angulo Movimento: ' + str(angle) + ' | Movimentos Incorretos: ' + str(incorrect_angle_counter)
-    image = cv2.putText(image, img_text, position, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
+        image = cv2.putText(image, img_text, position, font, fontScale, color, thickness, cv2.LINE_AA)
     out.write(image)
 
 pose.close()
 cap.release()
 out.release()
+
+end_time = time.time()
+execution_time = end_time - start_time
+formatted_time = str(timedelta(seconds=int(execution_time)))
+print(f"Execution time: {formatted_time}")
